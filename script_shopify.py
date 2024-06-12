8997d46aea411038226ee5aeb52067c3549cc0da
Mathieu <mathieukohl@hotmail.com>
2024-06-12 15:02:58 +0200

    Add missing ActionChains import

--- script_shopify.py
+++ script_shopify.py
@@ -4,6 +4,8 @@ from flask_cors import CORS
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
